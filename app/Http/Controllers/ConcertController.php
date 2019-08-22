d0cecbf37c184f15df04e0e2e31d727391e22f55
munzir1996 <munzirmukhtar996@gmail.com>
2019-08-22 13:51:28 +0200

    V1.0

---
+++ app/Http/Controllers/ConcertController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Concert;
+use Illuminate\Http\Request;
+
+class ConcertController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Concert  $concert
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $concert = Concert::published()->findOrFail($id);
+        // dd($concert);
+        return view('concerts.show', [
+            'concert' => $concert,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Concert  $concert
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Concert $concert)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Concert  $concert
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Concert $concert)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Concert  $concert
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Concert $concert)
+    {
+        //
+    }
+}
